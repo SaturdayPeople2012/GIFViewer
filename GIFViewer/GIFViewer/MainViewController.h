66823600a34f1714f60878e5fa97859d4f6aac50
양원석 <redsubmarine>
2012-10-21 11:46:47 +0900

    진짜 요이땅!!

--- GIFViewer/GIFViewer/MainViewController.h
+++ GIFViewer/GIFViewer/MainViewController.h
@@ -2,8 +2,8 @@
 //  MainViewController.h
 //  GIFViewer
 //
-//  Created by 양원석바보 on 12. 10. 21..
-//  Copyright (c) 2012년 양원석바보. All rights reserved.
+//  Created by 양원석 on 12. 10. 21..
+//  Copyright (c) 2012년 양원석. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
