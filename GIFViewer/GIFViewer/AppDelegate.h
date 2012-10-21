fee1dd7fc8eddc06f5a0e8c3a16b9ab885fb1d5a
양원석 <redsubmarine>
2012-10-21 11:10:00 +0900

    Init 양군

---
+++ GIFViewer/GIFViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GIFViewer
+//
+//  Created by 양원석 on 12. 10. 21..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
