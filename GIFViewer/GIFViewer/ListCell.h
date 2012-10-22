49455cc787a884ea0dd68d7b72807dde16e1e77a
Cruz <onlyhappy77@naver.com>
2012-10-23 05:34:31 +0900

    UITableCell 상속받은 ListCell 생성

---
+++ GIFViewer/GIFViewer/ListCell.h
@@ -0,0 +1,13 @@
+//
+//  ListCell.h
+//  GIFViewer
+//
+//  Created by Cruz on 12. 10. 23..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListCell : UIView
+
+@end
---
+++ GIFViewer/GIFViewer/ListCell.m
@@ -0,0 +1,31 @@
+//
+//  ListCell.m
+//  GIFViewer
+//
+//  Created by Cruz on 12. 10. 23..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import "ListCell.h"
+
+@implementation ListCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
