08b46d1321e0c65ca5dae2838d0b72a09637e341
태상 조 <anyway_do_it@naver.com>
2012-10-24 00:13:29 +0900

    GIFDetailViewController에 편집,기능,삭제 버튼 추가

--- GIFViewer/GIFViewer/GIFDetailViewController.m
+++ GIFViewer/GIFViewer/GIFDetailViewController.m
@@ -30,10 +30,41 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 
+//    UIBarButtonItem *editBtn = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemEdit
+//                                                                            target:self action:@selector(goEdit:)];
+    UIBarButtonItem *editBtn = [[UIBarButtonItem alloc]initWithTitle:@"편집" style:UIBarButtonItemStyleBordered
+                                                              target:self action:@selector(goEdit:)];
+    UIBarButtonItem *funcBtn = [[UIBarButtonItem alloc]initWithTitle:@"기능" style:UIBarButtonItemStyleBordered
+                                                              target:self action:@selector(goFunc:)];
+    UIBarButtonItem *deleteBtn = [[UIBarButtonItem alloc]initWithTitle:@"삭제" style:UIBarButtonItemStyleBordered
+                                                              target:self action:@selector(goDelete:)];
+    UIBarButtonItem *flexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:
+                                 UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    self.navigationItem.rightBarButtonItem = editBtn;
+    self.toolbarItems = [NSArray arrayWithObjects:funcBtn, flexible, deleteBtn, nil];
+    
+    ///////////////////////////////////////////////////////////////////////////////////////    
+    
     NSURL 			* gifUrl = 		 [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"apple_logo_animated" ofType:@"gif"]];
     UIImageView 	* gifAnimation = [AnimatedGif getAnimationForGifAtUrl: gifUrl];
     
     [gifPlayer addSubview:gifAnimation];
+}
+
+- (void)goEdit:(id)sender
+{
+    
+}
+
+- (void)goFunc:(id)sender
+{
+    
+}
+
+- (void)goDelete:(id)sender
+{
+    
 }
 
 - (void)didReceiveMemoryWarning
