a31dd24c54b67a90cff568b4cd1ddb1037b37e7b
태상 조 <anyway_do_it@naver.com>
2012-10-27 23:37:20 +0900

    다시 커밋

---
+++ GIFViewer/GIFViewer/GifLoadViewController.m
@@ -0,0 +1,60 @@
+//
+//  GifLoadViewController.m
+//  GIFViewer
+//
+//  Created by Kimyoung Lee on 12. 10. 27..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import "GifLoadViewController.h"
+
+@interface GifLoadViewController ()
+
+@end
+
+@implementation GifLoadViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self useCameraRoll];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) useCameraRoll
+{
+    if ([UIImagePickerController isSourceTypeAvailable:
+         UIImagePickerControllerSourceTypeSavedPhotosAlbum])
+    {
+        UIImagePickerController *imagePicker =
+        [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+        imagePicker.sourceType =
+        UIImagePickerControllerSourceTypePhotoLibrary;
+        imagePicker.mediaTypes = [NSArray arrayWithObjects:
+                                  (NSString *) kUTTypeImage,
+                                  nil];
+        imagePicker.allowsEditing = NO;
+        [self presentViewController:imagePicker animated:YES completion:nil];
+        //[self presentModalViewController:imagePicker animated:YES];
+
+//        [imagePicker release];
+        newMedia = NO;
+    }
+}
+@end
