a514374b6660b3f3f768863edbb63ee3c7a0ad64
KimyoungLee <lkmdilook@gmail.com>
2012-10-27 19:47:33 +0900

    init gif load class.

---
+++ GIFViewer/GIFViewer/GifLoadViewController.h
@@ -0,0 +1,13 @@
+//
+//  GifLoadViewController.h
+//  GIFViewer
+//
+//  Created by Kimyoung Lee on 12. 10. 27..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GifLoadViewController : UIViewController
+
+@end
---
+++ GIFViewer/GIFViewer/GifLoadViewController.m
@@ -0,0 +1,38 @@
+//
+//  GifLoadViewController.m
+//  GIFViewer
+//
+//  Created by Kimyoung Lee on 12. 10. 27..
+//  Copyright (c) 2012년 양원석. All rights reserved.
+//
+
+#import "GifLoadViewController.h"
+
+@interface GifLoadViewController ()
+
+@end
+
+@implementation GifLoadViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
