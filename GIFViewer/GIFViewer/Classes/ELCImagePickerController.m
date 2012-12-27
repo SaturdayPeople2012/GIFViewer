46c7fb27b6a64fb5c696366cbe450f43f3b3943c
KimyoungLee <lkmdilook@gmail.com>
2012-12-28 01:57:07 +0900

    읽은 파일을 document디렉토리에 저장하는 함수 추가.

--- GIFViewer/GIFViewer/Classes/ELCImagePickerController.m
+++ GIFViewer/GIFViewer/Classes/ELCImagePickerController.m
@@ -22,6 +22,10 @@
 	}
 }
 
+-(void)selectedAssetsNames:(NSArray*)_assets {
+    
+}
+
 -(void)selectedAssets:(NSArray*)_assets {
 
 	NSMutableArray *returnArray = [[[NSMutableArray alloc] init] autorelease];
@@ -32,7 +36,45 @@
 		[workingDictionary setObject:[asset valueForProperty:ALAssetPropertyType] forKey:@"UIImagePickerControllerMediaType"];
         [workingDictionary setObject:[UIImage imageWithCGImage:[[asset defaultRepresentation] fullScreenImage]] forKey:@"UIImagePickerControllerOriginalImage"];
 		[workingDictionary setObject:[[asset valueForProperty:ALAssetPropertyURLs] valueForKey:[[[asset valueForProperty:ALAssetPropertyURLs] allKeys] objectAtIndex:0]] forKey:@"UIImagePickerControllerReferenceURL"];
-		
+        
+        //lkm - add filename {
+        NSString* originalFileName = [[asset defaultRepresentation] filename];
+        [workingDictionary setObject:originalFileName forKey:@"UIImagePickerControllerMediaMetadata"];
+        //lkm - add filename }
+        
+        //lkm - save file{
+        ALAssetRepresentation *representation = [asset defaultRepresentation];
+        NSError *error;
+        //참고: http://stackoverflow.com/questions/6686730/iphone-how-does-one-read-an-image-from-the-photo-library-as-nsdata
+        // 근데 이레저레 좀 만졌다. 
+        //블록 크기만큼 바이트를 만든다.
+        uint8_t *bytes = malloc([representation size]);
+        //바이트를 로드하고, 바이트블록의 길이를 알아낸다.
+        NSUInteger length = [representation getBytes:bytes fromOffset:0 length:[representation size] error:&error];
+
+        //NSData타입 형태로 변환한다.
+        NSData *data = [NSData dataWithBytes:bytes length:length];
+        
+        NSString *docsDir;
+        NSArray *dirPaths;
+        
+        // documents 경로를 얻는다.
+        dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                       NSUserDomainMask, YES);
+        
+        docsDir = [dirPaths objectAtIndex:0];
+        BOOL ret = NO;
+#if 0
+        //TTT 이렇게 하면 nil이다. 못불러온다.
+        NSData *gifData = [NSData dataWithContentsOfURL:[[asset defaultRepresentation] url]];
+        ret = [gifData writeToFile:[docsDir stringByAppendingPathComponent:originalFileName] atomically:YES];
+        NSLog(@"ret : %d", ret);
+#endif
+        //저장한다.
+        ret = [data writeToFile:[docsDir stringByAppendingPathComponent:originalFileName] atomically:YES];
+        NSLog(@"ret2 : %d", ret);
+		//lkm - save file}
+        
 		[returnArray addObject:workingDictionary];
 		
 		[workingDictionary release];	
